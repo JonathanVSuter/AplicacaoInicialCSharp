7edeaef80e1f85c98f553a9c2795a8336a31910b
JONATHAN VINÍCIUS SUTER <jonathan.suter@multilog.com.br>
2022-04-27 22:20:21 -0300

    Laços de repetição e matriz.

---
+++ VetoresMatrizes/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace VetoresMatrizes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] vetorInteiro = new int[50];
+            char[] vetorChar = new char[10];
+
+
+
+        }
+    }
+}
